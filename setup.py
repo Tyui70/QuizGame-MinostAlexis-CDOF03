5575a2674169abe6f971833a07206482256da774
Tyui70 <118425755+Tyui70@users.noreply.github.com>
2024-01-22 17:45:31 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+ name='{General French Quiz Game}',
+ version='1.0',
+ author='{MINOST Alexis}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
